786f3fc1d04b71fc3595e245db6680419a16885d
Fabriciovich <fabriciocostadoni@gmail.com>
2023-10-05 11:12:26 -0300

    Se vinculo la vista con el servidor

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+
 app.use(express.json());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+
+// Configurar el motor de vistas EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'view'));
+app.use(express.static(path.join(__dirname, 'src/public')));
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    // Renderizar el archivo HTML utilizando EJS
+    res.sendFile(__dirname+'/src/view/index.html');
 });
 
 app.listen(port, () => {
-    console.log(`Server on port ${port}`);
-})
+    console.log(`Servidor en el puerto ${port}`);
+});
