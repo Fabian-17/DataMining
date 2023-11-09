bae7b7328bd007a2586ce0d69a382209f56a9325
Fabian <fabianvillagra28@gmail.com>
2023-11-08 21:02:09 -0300

    fix: se mejoro el modelado

--- src/models/model.js
+++ src/models/model.js
@@ -2,36 +2,61 @@ import { DataTypes } from 'sequelize';
 import { sequelize } from '../config/db.js';
 
 export const Genero = sequelize.define('genero', {
-    nombre: DataTypes.STRING,
+    nombre: {
+      type: DataTypes.STRING
+     },
   });
   
   export const Localidad = sequelize.define('localidad', {
-    nombre: DataTypes.STRING,
+    nombre: {
+      type: DataTypes.STRING
+     },
   });
   
   export const NivelEstudio = sequelize.define('nivel_estudio', {
-    nombre: DataTypes.STRING,
+    nombre: {
+     type: DataTypes.STRING
+    },
   });
   
   export const Encuesta = sequelize.define('encuesta', {
     // Otros atributos de la encuesta
-    edad: DataTypes.INTEGER,
+    edad: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+    },
   });
   
   export const Pregunta = sequelize.define('pregunta', {
     // Otros atributos de la pregunta
-    texto: DataTypes.STRING,
+    texto:{
+      type: DataTypes.STRING
+     },
   });
   
   export const Respuesta = sequelize.define('respuesta', {
     // Otros atributos de la respuesta
-    1: DataTypes.STRING,
-    2: DataTypes.STRING,
-    3: DataTypes.STRING,
-    4: DataTypes.STRING,
-    5: DataTypes.STRING,
-    6: DataTypes.STRING,
-    7: DataTypes.STRING,
+    1: {
+      type: DataTypes.STRING
+     },
+    2: {
+     type: DataTypes.STRING
+    },
+    3: {
+     type: DataTypes.STRING
+    },
+    4: {
+     type: DataTypes.STRING
+    },
+    5: {
+     type: DataTypes.STRING
+    },
+    6: {
+     type: DataTypes.STRING
+    },
+    7: {
+     type: DataTypes.STRING
+    },
   });
   
   export const RespuestaEncuesta = sequelize.define('respuesta_encuesta', {
